bce265ec5aa913c6e63e8c9f6aab3afe95695d3a
Carlos Cordoba <carloscba1@gmail.com>
2018-01-05 10:46:35 -0300

    - v1.0.14

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 export default function(code, page, path = '../../../locale'){
-
-    const localePath = `${path}/${code}.js`
-    
     try{
-        const response = require(localePath)
-        const data = response.default
-        return data[page] ;
+        const response = require(`${path}/${code}.js`)
+        return response.default[page];
     }catch(error){
         new Error('Appolodoro:' + error)
-    } 
-     
+    }
 };
